4ec911b07b59b76f7c80b04d17e9626d489b4f46
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2018-04-11 11:40:56 -0300

    commit

--- spec/factories/recurring_events.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :recurring_event do
-    title "MyString"
-    anchor "2018-04-09"
-    frequency 1
-    color "MyString"
-  end
-end
--- test/system/recurring_events_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class RecurringEventsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit recurring_events_url
-  #
-  #   assert_selector "h1", text: "RecurringEvent"
-  # end
-end
